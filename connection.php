22e48186036bcdc19abd70c0e095391acbc5e7bc
benbue <benbue@users.noreply.github.com>
2017-08-09 01:08:09 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $hostname_connection = "localhost";
-$database_connection = "usr_web197_4";
+$database_connection = "####";
 $username_connection = "esv";
-$password_connection = "2rKAAPsMG9dMG36u";
+$password_connection = "#####";
 //$password_connection = "Q5knupPr";
 $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
 // echo ($database_connection);
